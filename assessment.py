bca50b305791135e9a01172d0deb7a5e35925085
vagrant <vagrant@vagrant.vm>
2017-03-05 19:34:55 +0000

    Initial file skeleton

---
+++ assessment.py
@@ -0,0 +1,27 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+   -Encapsulation: Data lives close to its functionality, which becomes particularly useful when building larger programs
+   -Abstraction: We only need to understand what a method does in order to use it, and allows us to disociate this from how the method does it
+   -Polymorphism: Allows us to make different interchangable types of an object (interchangabability of components)
+
+2. What is a class?
+
+3. What is an instance attribute?
+
+4. What is a method?
+
+5. What is an instance in object orientation?
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
